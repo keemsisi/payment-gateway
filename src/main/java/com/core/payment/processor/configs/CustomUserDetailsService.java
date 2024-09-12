05f480167ad33ddc320a772081da97018e4078b6
Adeshina <91054814+integral1@users.noreply.github.com>
2024-09-12 04:19:25 +0100

    test updated

---
+++ src/main/java/com/core/payment/processor/configs/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.core.payment.processor.configs;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if ("user".equals(username)) {
+            return User.withUsername("user")
+                    .password("password")
+                    .roles("USER")
+                    .authorities("CARD_TRANSFER", "WALLET_TRANSFER",
+                            "BANK_TRANSFER", "CREATE_BANK", "DELETE_BANK",
+                            "UPDATE_WALLET", "CREATE_WALLET", "DELETE_WALLET")
+                    .build();
+        } else {
+            throw new UsernameNotFoundException("User not found");
+        }
+    }
+}
--- src/main/java/com/core/payment/processor/configs/SecurityConfig.java
+++ src/main/java/com/core/payment/processor/configs/SecurityConfig.java
@@ -1,12 +1,15 @@
 package com.core.payment.processor.configs;
 
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @AllArgsConstructor
@@ -14,6 +17,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final CustomAuthenticationEntryPoint entryPoint;
+    private final CustomUserDetailsService customUserDetailsService;
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
@@ -22,20 +38,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/v1/api/**").authenticated()
                 .anyRequest().permitAll()
-                .and().formLogin()
-                .permitAll().and()
-                .httpBasic().authenticationEntryPoint(entryPoint);
+                .and()
+                .httpBasic();
     }
 
-    @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("user")
-                .password("{noop}password")
-                .roles("USER")
-                .authorities("CARD_TRANSFER", "WALLET_TRANSFER",
-                        "BANK_TRANSFER", "CREATE_BANK", "DELETE_BANK",
-                        "UPDATE_WALLET", "CREATE_WALLET", "DELETE_WALLET");
-    }
 }
