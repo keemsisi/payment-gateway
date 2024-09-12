86f5a251dc1911b978be201a390aa15f13b252f0
Adeshina <91054814+integral1@users.noreply.github.com>
2024-09-12 05:15:08 +0100

    updated codes

--- src/main/java/com/core/payment/processor/configs/CustomUserDetailsService.java
+++ src/main/java/com/core/payment/processor/configs/CustomUserDetailsService.java
@@ -5,7 +5,6 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,7 +12,7 @@ import org.springframework.stereotype.Service;
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
         if ("user".equals(username)) {
             return User.withUsername("user")
                     .password("password")
